2e0e36c6e1187df0e82767f3d5f3d4cdd2324dcc
TimofeiKorchakovsky <korc2017@mail.ru>
2024-01-02 12:59:08 +0300

    add lab1

---
+++ Lab1/creator.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+struct employee
+{
+    int num;
+    char name[10];
+    double hours;
+};
+
+int main(int argc, char* argv[])
+{
+
+    const char* file_name = argv[1];
+    int countEmployee = std::stoi(argv[2]);
+
+    std::ofstream outputFileStream(file_name, std::ios::binary);
+    if (!outputFileStream.is_open())
+    {
+        std::cerr << "The file could not be opened for writing." << std::endl;
+        return 1;
+    }
+
+    for (int i = 0; i < countEmployee; ++i)
+    {
+        employee emp;
+
+        std::cout << "Enter id of employee: ";
+        std::cin >> emp.num;
+        std::cout << "Enter name of employee: ";
+        std::cin >> emp.name;
+        std::cout << "Enter number of hours of employee: ";
+        std::cin >> emp.hours;
+
+        outputFileStream.write((char*)&emp, sizeof(employee));
+    }
+
+    outputFileStream.close();
+    std::cout << "The file was created successfully." << std::endl;
+
+    return 0;
+}
---
+++ Lab1/reporter.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string> 
+
+struct employee
+{
+    int num;        
+    char name[10];  
+    double hours;   
+};
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "Using: " << argv[0] << " binaryFile reportName moneyPerHour" << std::endl;
+        return 1;
+    }
+
+    const char* binaryFile = argv[1];
+    const char* reportName = argv[2];
+    double doubleMoneyPerHour = std::stod(argv[3]);
+
+    std::ifstream in(binaryFile, std::ios::binary);
+    if (!in.is_open()) {
+        std::cerr << "The binary file could not be opened." << std::endl;
+        return 1;
+    }
+
+    std::ofstream out(reportName);
+    if (!out.is_open()) {
+        std::cerr << "The report file could not be created." << std::endl;
+        return 1;
+    }
+
+    while (true) {
+        employee emp;
+        in.read((char*)&emp, sizeof(employee));
+        if (in.eof())
+            break;
+
+        out << emp.num << " " << emp.name << " " << emp.hours << " " << doubleMoneyPerHour * emp.hours << '\n';
+    }
+
+    in.close();
+    out.close();
+
+    return 0;
+}
